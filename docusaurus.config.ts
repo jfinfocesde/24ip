57af86ff9e6f9673aad46619c959663f882b48df
Jhon Fredy <jfinfocesde@gmail.com>
2024-08-07 13:36:13 -0500

    2024-8-7

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'NTP',
-  tagline: 'Dinosaurs are cool',
+  title: 'IP',
+  tagline: 'IP',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
